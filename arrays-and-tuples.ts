2a3d0e3142fe30cbc70cfcaf07559e83a3e9c7f7
ccbusby123 <137566179+ccbusby123@users.noreply.github.com>
2023-09-24 19:40:51 -0500

    Create arrays-and-tuples.ts
    Created an array named subjects and declared a tuple named student

---
+++ arrays-and-tuples.ts
@@ -0,0 +1,3 @@
+let subjects: string[] = ["Algebra", "Biology", "Spanish"];
+let student: [string, number] = ["John", 20];
+//In TypeScript, a tuple is a data structure that allows you to store a fixed number of elements of different types in a specific order.
